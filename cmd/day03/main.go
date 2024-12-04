04be97d84d12fed383c9ea59cb4965f71ba8a279
Mathias Kluge <59650843+mathiaskluge@users.noreply.github.com>
2024-12-04 08:54:02 +0100

    Day03 - Part 2

--- cmd/day03/main.go
+++ cmd/day03/main.go
@@ -21,6 +21,10 @@ func main() {
 	// Join data into a single string
 	dataString := strings.Join(data, "")
 
+	// Part 1:
+	// Find all mul(X,X) where X is a 1-3 digit number.
+	// Multiply X*X and add to a total sum
+
 	// find matches and put in format [["mul(123,456)", "123", "456"]]
 	regex := `mul\((\d{1,3}),(\d{1,3})\)`
 	r := regexp.MustCompile(regex)
@@ -32,21 +36,73 @@ func main() {
 	for _, match := range matches {
 		// fmt.Printf("%v * %v\n", match[1], match[2])
 
-		factor1, err := strconv.Atoi(match[1])
+		product, err := mulStrings(match[1], match[2])
 		if err != nil {
-			fmt.Println("Error converting factor1:", err)
+			fmt.Println("Error multiplying strings:", err)
 			return
 		}
-		factor2, err := strconv.Atoi(match[2])
-		if err != nil {
-			fmt.Println("Error converting factor2:", err)
-			return
-		}
-		sum += int64(factor1) * int64(factor2)
+		sum += int64(product)
 		// fmt.Printf("%v \n", sum)
 	}
 
-	fmt.Printf("Final sum: %v\n", sum)
+	fmt.Printf("Part 1: %v\n", sum)
+
+	// Part 2:
+	// There are do() and don't() statements in the data.
+	// Only compute the mul(X,X) after a do() block.
+	// Skip computation in a don't() block.
+
+	// adds 2 additional groups in front for do() and don't()
+	regex = `(do\(\))|(don't\(\))|mul\((\d{1,3}),(\d{1,3})\)`
+	r = regexp.MustCompile(regex)
+
+	matches = r.FindAllStringSubmatch(dataString, -1)
+
+	mulEnabled := true
+	var sum2 int64
+
+	for _, match := range matches {
+
+		if mulEnabled && match[2] == "don't()" {
+			mulEnabled = false
+			// fmt.Printf("[2]: %v --> mulling disabled\n", match[2])
+		}
+
+		if mulEnabled && match[3] != "" && match[4] != "" {
+
+			product, err := mulStrings(match[3], match[4])
+			if err != nil {
+				fmt.Println("Error multiplying strings:", err)
+				return
+			}
+
+			sum2 += int64(product)
+
+			// fmt.Printf("[3]: %v * [4]: %v ---> Current Sum: %v \n", match[3], match[4], sum2)
+
+		}
+
+		if !mulEnabled && match[1] == "do()" {
+			mulEnabled = true
+			// fmt.Printf("[1]: %v --> mulling enabled\n", match[1])
+		}
+
+	}
+
+	fmt.Printf("Part 2: %v\n", sum2)
+
+}
+
+func mulStrings(a, b string) (int, error) {
+	aInt, err := strconv.Atoi(a)
+	if err != nil {
+		return 0, err
+	}
+	bInt, err := strconv.Atoi(b)
+	if err != nil {
+		return 0, err
+	}
+	return aInt * bInt, nil
 }
 
 func readData(filename string) ([]string, error) {
