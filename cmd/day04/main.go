133b8cf18c6aa966aaddaa8e36acb210885abeb7
Mathias Kluge <59650843+mathiaskluge@users.noreply.github.com>
2024-12-05 15:54:28 +0100

    Day04

--- cmd/day04/main.go
+++ cmd/day04/main.go
@@ -23,30 +23,63 @@ func main() {
 	var matchCount int
 
 	matchCount += horizontalMatch(wordMatrix)
-	fmt.Printf("Horizontal matches: %d\n", matchCount)
+	fmt.Printf("0 Degrees: %d\n", matchCount)
 	matchCount += horizontalMatch(wordMatrix90)
-	fmt.Printf("With Vertical (rotate 90 degrees) matches: %d\n", matchCount)
+	fmt.Printf("90 Degrees: %d\n", matchCount)
 	matchCount += horizontalMatch(wordMatrix45)
-	fmt.Printf("With Diagonal Positive matches: %d\n", matchCount)
+	fmt.Printf("45 Degrees: %d\n", matchCount)
 	matchCount += horizontalMatch(wordMatrix135)
-	fmt.Printf("With Diagonal Negative matches: %d\n", matchCount)
+	fmt.Printf("135 Degrees: %d\n", matchCount)
 
+	fmt.Printf("\n Matching X-MAS patterns\n")
+
+	matchCount = patternMatch(wordMatrix)
+	fmt.Println(matchCount)
+}
+
+func patternMatch(matrix [][]rune) int {
+	var matchCount int
+
+	// limits at [:-1][:-1] since X is build from the center letter
+	for i := 1; i < len(matrix)-1; i++ {
+		for j := 1; j < len(matrix[i])-1; j++ {
+
+			// every potentioal match has a center 'A'
+			if matrix[i][j] != 'A' {
+				continue
+			}
+
+			leftLeg := string([]rune{
+				matrix[i-1][j-1], // X . .
+				matrix[i][j],     // . j .
+				matrix[i+1][j+1], // . . X
+			})
+			rightLeg := string([]rune{
+				matrix[i-1][j+1], // . . X
+				matrix[i][j],     // . j .
+				matrix[i+1][j-1], // X . .
+			})
+
+			if (leftLeg == "MAS" || leftLeg == "SAM") && (rightLeg == "MAS" || rightLeg == "SAM") {
+				matchCount++
+			}
+		}
+	}
+
+	return matchCount
 }
 
 func horizontalMatch(matrix [][]rune) int {
 	var matchCount int
 
 	for _, row := range matrix {
+		if len(row) < 4 {
+			continue
+		}
 		for x := 0; x <= len(row)-4; x++ {
-			// Forward Match
-			if string(row[x:x+4]) == "XMAS" {
+			substring := string(row[x : x+4])
+			if substring == "XMAS" || substring == "SAMX" {
 				matchCount++
-				//fmt.Println(string(row[x : x+4]))
-			}
-			// Backward Match
-			if x >= 4 && string(row[x-4:x]) == "SAMX" {
-				matchCount++
-				//fmt.Println(string(row[x-4 : x]))
 			}
 		}
 	}
